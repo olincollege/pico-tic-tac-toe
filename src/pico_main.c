e5eea9d9fe02b700518fa631047b8400f76e9559
drewnotdrew <silverbird337@gmail.com>
2024-04-28 15:18:18 -0400

    Add testing changes

--- src/pico_main.c
+++ src/pico_main.c
@@ -18,12 +18,12 @@ static void heartbeat_handler(struct btstack_timer_source* ts) {
   counter++;
 
   // Update the temp every 10s
-  if (counter % 1 == 0) {
-    poll_temp();
-    if (le_notification_enabled) {
-      att_server_request_can_send_now_event(con_handle);
-    }
-  }
+  // if (counter % 1 == 0) {
+  //   poll_temp();
+  //   if (le_notification_enabled) {
+  //     att_server_request_can_send_now_event(con_handle);
+  //   }
+  // }
 
   // Invert the led
   static int led_on = true;
@@ -72,6 +72,6 @@ int main(void) {
   hci_power_control(HCI_POWER_ON);
 
   while (true) {
-    sleep_ms(1000);
+    // sleep_ms(1000);
   }
 }
