7758c2ce4ab49a423e3456c2fa62fdbda6612575
drewnotdrew <silverbird337@gmail.com>
2024-04-28 02:01:17 -0400

    Working increment, data is valid

--- src/bt_server.c
+++ src/bt_server.c
@@ -148,7 +148,8 @@ void poll_temp(void) {
   // The temperature sensor measures the Vbe voltage of a biased bipolar diode,
   // connected to the fifth ADC channel Typically, Vbe = 0.706V at 27 degrees C,
   // with a slope of -1.721mV (0.001721) per degree.
-  float deg_c = 27 - (reading - 0.706) / 0.001721;
+  static float deg_c = 17.69;
+  deg_c++;
   current_temp = deg_c * 100;
   printf("Write temp %.2f degc\n", deg_c);
 }
