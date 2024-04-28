76ff2d714cd6758a755448985eac2489547b8e08
drewnotdrew <silverbird337@gmail.com>
2024-04-28 16:08:58 -0400

    Remove old adc temp code

--- src/bt_server.c
+++ src/bt_server.c
@@ -142,22 +142,6 @@ int att_write_callback(hci_con_handle_t connection_handle, uint16_t att_handle,
 }
 
 void poll_temp(void) {
-  adc_select_input(ADC_CHANNEL_TEMPSENSOR);
-  uint32_t raw32 = adc_read();
-  const uint32_t bits = 12;
-
-  // Scale raw reading to 16 bit value using a Taylor expansion (for 8 <= bits
-  // <= 16)
-  uint16_t raw16 = raw32 << (16 - bits) | raw32 >> (2 * bits - 16);
-
-  // ref
-  // https://github.com/raspberrypi/pico-micropython-examples/blob/master/adc/temperature.py
-  const float conversion_factor = 3.3 / (65535);
-  float reading = raw16 * conversion_factor;
-
-  // The temperature sensor measures the Vbe voltage of a biased bipolar diode,
-  // connected to the fifth ADC channel Typically, Vbe = 0.706V at 27 degrees C,
-  // with a slope of -1.721mV (0.001721) per degree.
   static uint8_t deg_c = 3;
   deg_c++;
   current_temp = deg_c;
@@ -196,11 +180,6 @@ int main(void) {
     printf("failed to initialise cyw43_arch\n");
     return -1;
   }
-
-  // Initialise adc for the temp sensor
-  adc_init();
-  adc_select_input(ADC_CHANNEL_TEMPSENSOR);
-  adc_set_temp_sensor_enabled(true);
 
   l2cap_init();
   sm_init();
